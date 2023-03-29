807f67853ee4f9d7ab0e4c1094af0067e2094a03
whitejsx <orbittechnologyinc@gmail.com>
2023-03-29 08:21:18 -0700

    Fetch data from supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react'
-
 import { supabase } from "./createClient"
+import './App.css'
 
 const App = () => {
 
@@ -25,7 +25,27 @@ const App = () => {
 
   return (
     <div>
-      Application
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+          users.map((user) => 
+            <tr>
+              <th>{user.id}</th>
+              <th>{user.name}</th>
+              <th>{user.age}</th>
+              <th>{user.created_at}</th>
+            </tr>
+          )}
+        </tbody>
+      </table>
       </div>
   )
 }
