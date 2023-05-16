57e39718ed879d20096141fd1d4cf4feb88e416a
whitejsx <orbittechnologyinc@gmail.com>
2023-05-16 05:54:50 -0700

    fix buggy

--- src/App.jsx
+++ src/App.jsx
@@ -532,58 +532,14 @@ function App() {
       {/* footer */}
       <footer class="p-4 bg-slate-700 mt-20 sm:p-6 dark:bg-gray-800 text-white">
     <div class="mx-auto max-w-screen-xl">
-        <div class="md:flex md:justify-between">
-            <div class="mb-6 md:mb-0">
-                <div>
-                <a href="#" class="flex items-center">
-                    <img src={Logo} class="mr-3 h-8" alt="FlowBite Logo" />
-                  <p>Pips Catchers</p>
-                </a>
-                </div>
-            </div>
-            {/* <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3 text-white">
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
-                    <ul class="text-gray-600 dark:text-gray-400">
-                        <li class="mb-4">
-                         
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline"></a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                    <ul class="text-gray-600 dark:text-gray-400">
-                        <li class="mb-4 text-white">
-                            <a href="#" class="hover:underline ">twitter</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline text-white">Discord</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline text-white">Whatsapp</a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                    <ul class="text-gray-600 dark:text-gray-400">
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                        </li>
-                    </ul>
-                </div>
-            </div> */}
-        </div>
+        
         
         <div class="sm:flex sm:items-center sm:justify-between ">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="" class="hover:underline">PIPsCatcher</a>. All Rights Reserved.
-            </span>
+            <div class="text-sm text-gray-500 sm:text-center dark:text-gray-400 flex items-center font-bold">© 2023
+             <img src={Logo} class="mr-3 h-8 p-1" alt="FlowBite Logo" />
+                  <p>Pips Catchers</p>
+             <a href="" class="hover:underline">PIPsCatcher</a>. All Rights Reserved.
+            </div>
             <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0 text-white">
                 <a href="#" class="text-gray-50 hover:text-gray-900 dark:hover:text-white">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
